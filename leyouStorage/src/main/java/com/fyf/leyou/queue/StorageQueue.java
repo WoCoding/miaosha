72ac1ec0dc372c952321c12bbc9ade72b28c8665
QUT201707296 <510189247@qq.com>
2020-12-15 19:01:21 +0800

    Initial commit

--- leyouStorage/src/main/java/com/fyf/leyou/queue/StorageQueue.java
+++ leyouStorage/src/main/java/com/fyf/leyou/queue/StorageQueue.java
@@ -6,6 +6,7 @@ import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -18,28 +19,37 @@ import java.util.Map;
 public class StorageQueue {
     @Autowired
     private StorageService storageService;
-
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     //监听storage_queue队列,获取消息，执行方法
     @RabbitListener(queues = "storage_queue")
     public void updateStorage(Channel channel, @Payload String message, @Header(AmqpHeaders.DELIVERY_TAG) long tag,
-                              @Header(AmqpHeaders.REDELIVERED) boolean reDelivered ) throws IOException {
-
+                              @Header(AmqpHeaders.REDELIVERED) boolean reDelivered) throws IOException {
+        try {
             //输出消息
-            System.out.println("接收storage_queue消息："+message);
-            //根据msg,进行更新库存
-            Map<String,Object> resultMap=new HashMap<>();
-            resultMap=storageService.insertStorage(message,0,1);
-            //判断是否处理成功
-            if((Boolean) resultMap.get("result")){
-                System.out.println("消息处理成功！");
-                //手动确认
+            System.out.println("接收storage_queue消息：" + message);
+            //保证幂等性
+            if (stringRedisTemplate.opsForValue().get(String.valueOf(tag)) == null) {
+                //减库存
+                storageService.insertStorage(message, 0, 1);
+                stringRedisTemplate.opsForValue().set(String.valueOf(tag), "1");
                 channel.basicAck(tag, false);
-            }else {
-                System.out.println("storage_queue消息处理失败："+resultMap.get("msg").toString());
-                //重新入队一次
-                channel.basicNack(tag,false,true);
             }
+
+        } catch (Exception e) {
+            if (reDelivered) {
+                System.out.println("消息已重复处理失败：{}" + message);
+                channel.basicReject(tag, false);
+            } else {
+                System.out.println("消息处理失败" + e);
+                //重新入队一次
+                channel.basicNack(tag, false, true);
+            }
+
+        }
+
     }
+
 
 }
